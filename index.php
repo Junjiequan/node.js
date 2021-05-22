737917a1830067c2687f95729b707cf52279276d
mmmG <a331998513@gmail.com>
2021-05-22 23:40:11 +0200

    somehow I ended up practicing oop with JS -_-!

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="index.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <?php
-    
-     $FirstVariable = 'let\'s test it out';
-    echo $FirstVariable;
-    ?>
+    <h1>
+        <?php
+            $num1 = 5;
+            $num2 = 1;
+            $sum = $num1 + $num2;
+            echo $num2 <=> $num1;
+            $num1 !== $num2;
+        ?>
+    </h1>
 </body>
 </html>
