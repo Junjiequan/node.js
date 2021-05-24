6fdafd27cd16eefa43642ff1d98c0f92b338ec5d
mmmG <a331998513@gmail.com>
2021-05-24 23:41:55 +0200

    hmm

--- index.php
+++ index.php
@@ -33,16 +33,54 @@
         //     // foreach ($arr as $value){
         //     //     echo 'randomeaaaa' . $value;
         //     // }
-                $arrays = [
-                    "name" => 'oyea',
-                    "color" => 'blue'
-                ];
-                foreach ($arrays as $key => $value){
-                    echo $key . ":" . $value;
-                }
+                // $arrays = [
+                //     "name" => 'oyea',
+                //     "color" => 'blue'
+                // ];
+                // foreach ($arrays as $key => $value){
+                //     echo  $value ;
+                // }
+        // $a = 1;
+        // while ($a < 10){
+        //     $a++;
+        //     if ($a == 7){
+        //         continue;
+        //     }
+        //     echo $a;
+        // }
 
+        // include 'test.php';
+        // include_once 'test.php';
+        // require 'test.php';
+        // require_once 'test.php';
 
         ?>
+        //
+
+        <?php
+        ////////////////////////////built-in functions//////////////////////////
+            // $a = "hello guys!";
+            // $b = str_replace('hello', 'no-hello', $a);
+            // echo $b;
+
+            // $a = "number 3";
+            // $b = str_repeat('nooooo ' , 3);
+            // echo $b;
+
+            // $a = "what is this um?";
+            // $b = "what";
+            // echo stripos($a, 'w');
+
+
+            function getInfo($name,$game){
+                if($name === 'jay'){
+                    echo 'he loves to ' . 'play ' . $game;
+                };
+            };
+            getInfo('jay', 'php')
+        ?>
     </h1>
+    <br style="height:50px;">
+    <div>123213</div>
 </body>
 </html>
