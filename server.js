096101a331f979804f40ed5d7b022a9442ca8428
mmmG <a331998513@gmail.com>
2021-05-28 02:29:51 +0200

    all working

--- server.js
+++ server.js
@@ -5,37 +5,46 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('content-type','text/html');
-
+    res.setHeader('Content-Type','text/html');
     let path = './';
     switch(req.url){
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         case '/contact':
             path += 'contact.html';
+            res.statusCode = 200;
             break;
         case '/random':
             path += 'random.html';
+            res.statusCode = 200;
+            break;
+        case '/skills':
+            path += 'skills.html';
+            res.statusCode = 200;
             break;
         default:
-            path += 'index.html';
+            console.log(req.url);
+            res.setHeader('Location', '/');
+            res.statusCode = 301;
             break;
     }
     fs.readFile(path,(err,data)=>{
         if(err){
             console.error(err);
+            res.end();
         } else {
             res.end(data);
         }
     })
 })
 server.listen(PORT,HOSTNAME, ()=>{
-    console.log(`server running at http://${HOSTNAME}:${PORT} /`);
+    console.log(`server running at http://${HOSTNAME}:${PORT}/`);
 });
 // const newName = "delete.txt"
 // const { unlink } = require('fs');
